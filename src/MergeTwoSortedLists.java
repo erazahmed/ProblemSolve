7d5d2d08500bff4944f6d61fbd74d39ea58cfbde
Eraz Ahmed <eraz.ahmed@divergenttechbd.com>
2020-07-27 21:52:57 +0600

    Merge Two Sorted Lists (LeetCode Problem 23) revised

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -4,10 +4,6 @@ public class MergeTwoSortedLists {
 
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-		if (l1 == null && l2 == null) {
-			return null;
-		}
-
 		if (l1 == null) {
 			return l2;
 		}
